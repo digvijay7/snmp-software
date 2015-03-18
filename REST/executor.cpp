7d26b8293fc4940847ad5c7938a8d6433fff48dc
Digvijay Singh <digvijay09020@iiitd.ac.in>
2015-03-18 15:48:40 +0530

    Bug Fix

--- REST/executor.cpp
+++ REST/executor.cpp
@@ -280,7 +280,7 @@ bool Executor::count_at(const args_container &args, outputType type, string & re
   std::cout << "Making Query:"<<ss.str()<<std::endl;
   std::stringstream ss3;
   ss3 << "SELECT "<<ss2.str()<<",client_id FROM at_all_uids('"<<args.at<<"','"<<args.format<<"') ";
-  ss3 << "join label on device_id = uid GROUP BY";
+  ss3 << "join label on device_id = uid GROUP BY ";
   ss3 << ss2.str() << "ORDER BY "<< ss2.str()<<";";
 
   pqxx::result res1,res2;
